1f265abe182e1d237277a46e64031d1936c1306c
devopsotrator <devopsotrator@imp.to>
2019-12-28 22:13:38 +0300

    first commit

---
+++ .all.sh
@@ -0,0 +1,17 @@
+#! bin/bash
+
+# source files
+# source "$DOTFILES/.paths.sh"
+source "$DOTFILES/.aliases.sh"
+source "$DOTFILES/.functions.sh"
+
+# $include "$DOTFILES/.inputrc" # doesn't work on Mac
+
+# export all functions
+# causes sub bash calls (like with xargs) throw errors
+# eval "$(declare -F | sed -e 's/-f /-fx /')"
+
+# tmp here
+#if [ -f ~/.bash_completion ]; then
+#   . ~/.bash_completion
+#fi
